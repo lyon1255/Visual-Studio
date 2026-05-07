6c796880ae53ab77b2e9237251061e2650948937
lyon1255 <takacs.gergo2000@gmail.com>
2026-05-08 00:12:26 +0200

    Remove ServiceRoles and fix appsettings formatting
    Delete unused Infrastructure/ServiceRoles.cs which contained role constant definitions. Also apply minor formatting changes to GnosisAuthServer/appsettings.json (trailing whitespace adjustments).

--- GnosisAuthServer/GnosisAuthServer/Infrastructure/ServiceRoles.cs
+++
@@ -1,9 +0,0 @@
-namespace GnosisAuthServer.Infrastructure;
-
-public static class ServiceRoles
-{
-    public const string OfficialRealmHeartbeatWrite = "official-realm-heartbeat.write";
-    public const string CommunityRealmHeartbeatWrite = "community-realm-heartbeat.write";
-    public const string RealmGameDataRead = "realm-gamedata.read";
-    public const string RealmSchemaRead = "realm-schema.read";
-}
